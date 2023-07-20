499b7d47ae1fa0673513b3b20435fb670026c6b1
collin-cole <collinmc33@gmail.com>
2023-07-20 12:48:19 -0400

    all assets imported

---
+++ Assets/AssetStore/Solar System Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+    [Tooltip("This is the object the camera will look at when this object is clicked, defaults to the object this script is attached to")]
+	public GameObject target; // the target that the camera should look at
+
+    // Start is a Unity Specific function that runs when the script is created in the actual game, so on startup, or when loading into a scene
+    // or when an object with a script on it is created while the game is playing, etc.
+    //
+    // This start script handles the defaulting of the target variable if it has not been set
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+
+	// Called when MouseDown on this gameObject (The game object is clicked on in the game)
+	void OnMouseDown () {
+        // change the target of the LookAtTarget script to be this gameobject or whatever the target was set to.
+        LookAtTarget cmp = Camera.main.GetComponent<LookAtTarget>();
+        cmp.target = target;
+        Camera.main.fieldOfView = 60;
+	}
+}
---
+++ Assets/AssetStore/Solar System Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTarget : MonoBehaviour {
+
+	public GameObject target; // the target that the camera should look at
+
+	void Start () {
+		if (target == null) 
+		{
+            target = this.gameObject;
+			Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+	
+	// Update is called once per frame
+    // For clarity, Update happens constantly as your game is running
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray);
+            if (hits.Length>0)
+            {
+                RaycastHit hit = hits[0];
+                ChangeLookAtTarget cp = hit.collider.gameObject.GetComponent<ChangeLookAtTarget>();
+
+                if (cp!=null)
+                {
+                    target = hit.collider.gameObject;
+                }
+            }
+        }
+
+        // This is an if statement. Code inside of it will only be run/executed if the if condition is met.
+        // In this case the if statement is checking to see if target has been set to something, if it has, then the code inside of it runs
+        if (target)
+        {
+            // transform here refers to the attached gameobject this script is on.
+            // the LookAt function makes a transform point it's Z axis towards another point in space
+            // In this case it is pointing towards the target.transform
+            transform.LookAt(target.transform);
+        }
+    }
+}
