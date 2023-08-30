49a92278a05076db7907ff7f772a085f8389603d
Jacob Dunn <dunnjacob19@gmail.com>
2023-08-30 19:08:50 -0400

    More dismemberment setup

---
+++ Assets/Scripts/Enemy/ArmDismember.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmDismember : MonoBehaviour
+{
+    
+    public GameObject fleshRight;
+    public GameObject fleshLeft;
+
+    public GameObject dismemberArmRight;
+    public GameObject dismemberArmleft;
+
+    public GameObject scaledArmRight;
+    public GameObject scaledArmLeft;
+
+
+    void Update()
+    {
+        // if (arm health == 0)
+
+        //Scale Forearm 1 right and left in the rig (scaledArmRight and scaledArmLeft) to 0,0,0
+
+        //Enable fleshRight and fleshLeft
+
+        // Instantiate dismemberArmRight and dismemberArmLeft
+
+    }
+}
