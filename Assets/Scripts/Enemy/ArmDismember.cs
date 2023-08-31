ec6fa588fe169e4fb08c62debc316858d80b4650
drewthemain <54377401+drewthemain@users.noreply.github.com>
2023-08-30 20:20:17 -0400

    Cleaned up arm dismemberment (still WIP) and turned enemies back on without wall enemies

--- Assets/Scripts/Enemy/ArmDismember.cs
+++ Assets/Scripts/Enemy/ArmDismember.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class ArmDismember : MonoBehaviour
 {
     [Header("References")]
+
+    [Tooltip("The fleshy bit appearing after dismemberment")]
     public GameObject fleshyBit;
+    [Tooltip("The arm that was dismembered")]
     public GameObject dismemberedArm;
+    [Tooltip("The normal arm to be turned off")]
     public GameObject scaledArm;
 
     [Header("Options")]
 
+    [Tooltip("The health of the arm before dismemberment")]
     public float health = 20;
 
+    /// <summary>
+    /// Damage taken from LimbCollider
+    /// </summary>
+    /// <param name="damage">The amount of damage taken</param>
     public void TakeDamage(float damage)
     {
         health -= damage;
@@ -23,14 +32,17 @@ public class ArmDismember : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Turn on/off the correct gameobjects for the effect
+    /// </summary>
     public void Dismember()
     {
         if (!fleshyBit.activeSelf)
         {
+            scaledArm.transform.localScale = new Vector3(0, 0, 0);
             fleshyBit.SetActive(true);
             dismemberedArm.SetActive(true);
             dismemberedArm.transform.parent = null;
-            scaledArm.transform.localScale = new Vector3(0, 0, 0);
         }
         else
         {
