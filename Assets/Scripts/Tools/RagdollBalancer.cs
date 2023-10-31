cf193c1d94c29f9a879a98d5dcf3c15b5015350d
Drew White <54377401+drewthemain@users.noreply.github.com>
2023-10-31 19:51:06 -0400

    Balanced ragdolls for animation blending

---
+++ Assets/Scripts/Tools/RagdollBalancer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RagdollBalancer : EditorWindow
+{
+    public GameObject normalParent;
+    public GameObject ragdollParent;
+
+    [MenuItem("Window/Ragdoll Balancer")]
+    public static void ShowWindow()
+    {
+        GetWindow<RagdollBalancer>("Ragdoll Balancer");
+    }
+
+    private void OnGUI()
+    {
+        normalParent = (GameObject) EditorGUILayout.ObjectField(normalParent, typeof(Object), true);
+        ragdollParent = (GameObject) EditorGUILayout.ObjectField(ragdollParent, typeof(Object), true);
+        if (GUILayout.Button("Balance"))
+        {
+            Balancer();
+        }
+    }
+
+    private void Balancer()
+    {
+        if (normalParent == null || ragdollParent == null)
+        {
+            Debug.Log("Missing parent objects!");
+        }
+
+        Transform[] normalTransforms = normalParent.GetComponentsInChildren<Transform>();
+        Transform[] ragTransforms = ragdollParent.GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in normalTransforms)
+        {
+            foreach (Transform ragChild in ragTransforms)
+            {
+                if (child.name == ragChild.name)
+                {
+                    ragChild.position = child.position;
+                    ragChild.rotation = child.rotation;
+                    ragChild.localScale = child.localScale;
+                }
+            }
+        }
+
+        Debug.Log("Done!");
+    }
+}
